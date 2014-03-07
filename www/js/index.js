b2a06f336bd4b0589c6108477bf76d036e4b0e1e
Suryeon Kim <rus1031@gmail.com>
2014-03-08 05:17:59 +0900

    Suryeon changes

--- www/js/index.js
+++ www/js/index.js
@@ -4,19 +4,59 @@
 function ChatView(app, element){
     this.app = app;
     this.element = element;
+	this.toolbar = $(element).next();
+	// Templates
+	this.msg_template = Handlebars.compile($('#my-message-template').html());
+	this.others_template = Handlebars.compile($('#others-message-template').html());
+	// Bind events
+	$(this.toolbar).find('.messageSendButton').on( "click", $.proxy( this.sendMessage, this));
 }
 ChatView.prototype.sendMessage = function(){
-    // Get data from form
-    
+    //get current time with am or pm
+	var date = new Date();
+	var hour = date.getHours();
+	hour = (hour + 24) % 24;
+	var mid = 'AM';
+	if (hour == 0) 
+		hour = 12;
+	else if (hour > 12) {
+		hour = hour % 12;
+		mid = 'PM';
+	}
+	var min = date.getMinutes();
+	if (min < 10) {
+		min = '0' + min;
+	}
+	
+	// Get data from form
+    var txt = $(this.toolbar).find('.form textarea').val();
+	
     // HTML add markup
-    
+	var chatroom = $(this.element).find('.chatroom');
+	// check whether txt is empty
+	if (txt.length > 0) {
+		chatroom.append(this.msg_template({
+			time: hour + ":" + min + " " + mid,
+			text: txt
+		}));
+	}
+	
     // Send message to the server
     this.app.sendMessage(txt, function(){
         // Success
+		// chatroom.append('.... ok');
     }, function(){
         // Fail
     });
 }
+ChatView.prototype.receiveMessage = function() {
+	// Get others' text from server
+	
+	
+	// HTML add markup
+	var chatroom = $(this.element).find('.chatroom');
+}
+
 
 // Main application
 var app = {
@@ -25,6 +65,7 @@ var app = {
         // this.my_profile_id
         // txt
         // AJAX
+		success();
     },
     
     // Create a profile given the data
@@ -57,10 +98,10 @@ var app = {
         // Show view depending on status
         
         // Tests
-        //this.chatView.sendMessage('aaaa');
+        //this.chat_view.sendMessage('aaaa');
     },
     
     initializeViews: function(){
-        this.chatView = ChatView(this, document.getElementById('chat_view'));
+        this.chat_view = new ChatView(this, document.getElementById('chat_view'));
     }
 };
