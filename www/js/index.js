b1cd22f0b9aafaae99516c907709df2eaca0ddd3
Ander Martínez Sánchez <ander.basaundi@gmail.com>
2014-03-07 22:03:22 -0700

    Basic main js

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,23 @@
 
-var app = {
+// Main application
+var app = {    
+    // Create a profile given the data
+    createProfile: function(profile){
+        if(this.my_profile || localStorage.my_profile_id){
+            throw 'Profile already exists';
+        }
+        var col = this.db.col('profiles');
+        localStorage.my_profile_id = col.insert(profile);
+        this.my_profile = profile;
+        // TODO: Send AJAX
+    },
+    
     // Application Constructor
     initialize: function() {
-        console.log('Running');
+        // Initialize DB
+        var con = new Chongo.Connection(localStorage);
+        this.db = con.db('buzzCat');
+        
+        // Show view depending on status
     }
 };
