c02c6f1a7fbe4fd0187649b99fd8683bbc7c7d3d
Ander Martínez Sánchez <ander.basaundi@gmail.com>
2014-03-09 09:36:04 -0600

    Virginia merge

--- www/js/map.js
+++ www/js/map.js
@@ -4,25 +4,34 @@ function MapView(app, element){
     this.element = element;
 	
 	// create a map in the "map" div, set the view to a given place and zoom
-	var map = L.map('map').setView([this.app.my_position.lat, this.app.my_position.lon], 13);
-	var self = this;
+	var map = L.map('map').setView([46, -116], 13);
+	this.map = map;
 	
-	this.app.GET("/chats/" + this.app.location_id + "/users", function(data){
-        self.users = data.length;
-		self.marker.bindPopup("You are in " + self.app.location_name + " with " + self.users + " more people").openPopup();
-    });
+	// add a marker in the given location, attach some popup content to it and open the popup
+	var marker = L.marker([0, 0]).addTo(map);
+		
+	this.marker = marker;
+	
+	this.circle = L.circle([0, 0], 10, {
+		color: 'red',
+		fillColor: '#f03',
+		fillOpacity: 0.5
+	}).addTo(map);
 	
 	// add an OpenStreetMap tile layer
 	L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
 		attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 	}).addTo(map);
-
-	// add a marker in the given location, attach some popup content to it and open the popup
-	var marker = L.marker([this.app.my_position.lat, this.app.my_position.lon]).addTo(map);	
-		
-	L.circle([this.app.my_position.lat, this.app.my_position.lon], 20, {
-		color: 'red',
-		fillColor: '#f03',
-		fillOpacity: 0.5
-	}).addTo(map);
+	
+	var self = this;
+	$(document).bind('LOCATION_READY', function(){
+		var pos = new L.LatLng(self.app.my_position.lat, self.app.my_position.lon);
+		self.map.setView(pos, 17);
+		self.app.GET("/chats/" + self.app.location_id + "/users", function(data){
+			self.users = data.length;
+			console.log("You are in " + self.app.location_name + " with " + self.users + " more people");
+			self.marker.setLatLng(pos).bindPopup("You are in " + self.app.location_name + " with " + self.users + " more people").openPopup();
+			self.circle.setLatLng(pos);
+		});		
+	});
 }
